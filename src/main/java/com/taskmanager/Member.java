d36c091bc2a21b9c1cab9476a579cc3d958f3999
MonEnnemi <ichhalteallesaus@gmail.com>
2016-06-10 01:19:10 +0300

    Добавление hibernate

--- src/main/java/com/taskmanager/Member.java
+++ src/main/java/com/taskmanager/Member.java
@@ -1,7 +1,12 @@
 package com.taskmanager;
 
+import javax.persistence.*;
 
+@Entity
+@Table
 public class Member {
+
+    private int id;
 
     private String name;
     private String login;
@@ -10,8 +15,59 @@ public class Member {
     private String role;
     private Boolean isOnline;
 
-    public void createMember() {
+    @Id
+    @GeneratedValue
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
 
+    @Column
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Column
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
